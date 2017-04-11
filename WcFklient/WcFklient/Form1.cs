300b5d3cab16c0103d1b6a8c8fae523b211b294d
xstor1 <vojtastor32@gmail.com>
2017-04-11 12:17:15 +0200

    Add files via upload

---
+++ WcFklient/WcFklient/Form1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WcFklient
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            ServiceReference1.Service1Client client = new ServiceReference1.Service1Client();
+            string name;
+            name = client.GetData(textBox1.Text);
+            label1.Text = name;
+         
+        }
+    }
+}
